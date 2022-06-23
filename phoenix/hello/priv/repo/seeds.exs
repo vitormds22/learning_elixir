a6bef1e1ae9999de0bb2b6240ab6b04152044598
Vitor Machado <machado@pop-os.localdomain>
2022-06-23 09:40:07 -0300

    feat: Context and generators

--- phoenix/hello/priv/repo/seeds.exs
+++ phoenix/hello/priv/repo/seeds.exs
@@ -1,11 +1,3 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Hello.Repo.insert!(%Hello.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+for title <- ["Home Improvement", "Power Tools", "Gardening", "Books"] do
+  {:ok, _} = Hello.Catalog.create_category(%{title: title})
+end
